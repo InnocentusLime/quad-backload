06853bf942af7a972463fbe5f4d1501682f79a5e
InnocentusLime <innocentuslime.help@gmail.com>
2025-06-01 10:42:41 +0300

    Add useful shorthands

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use macroquad::prelude::*;
+
 #[cfg(not(target_arch = "wasm32"))]
 mod native;
 #[cfg(target_arch = "wasm32")]
@@ -9,3 +11,20 @@ use native as platform;
 use wasm as platform;
 
 pub use platform::*;
+
+impl BackgroundLoader {
+    pub async fn load_image(&mut self, path: &str) -> Result<Image, macroquad::Error> {
+        let file = self.load_file(path).await?;
+        Image::from_file_with_format(file.as_slice(), None)
+    }
+
+    pub async fn load_texture(&mut self, path: &str) -> Result<Texture2D, macroquad::Error> {
+        let image = self.load_image(path).await?;
+        Ok(Texture2D::from_image(&image))
+    }
+
+    pub async fn load_string(&mut self, path: &str) -> Result<String, macroquad::Error> {
+        let file = self.load_file(path).await?;
+        Ok(String::from_utf8_lossy(&file).to_string())
+    }
+}
